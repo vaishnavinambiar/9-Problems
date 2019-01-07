5fe03c0763404ad48b6f2c642bc4aacfe43471e3
vaishnavinambiar <vaishnaviknambiar1996@gmail.com>
2019-01-07 17:20:37 +0530

    solving exercises

---
+++ people_cats_dogs.py
@@ -0,0 +1,18 @@
+people = raw_input("Enter the number of people: ")
+cats = raw_input("Enter the number of cats: ")
+dogs = raw_input("Enter the number of dogs: ")
+if people < cats:
+    print "Too many cats! The world is doomed!"
+if people > cats:
+    print "Not many cats! The world is saved!"
+if people < dogs:
+    print "The world is drooled on!"
+if people > dogs:
+    print "The world is dry!"
+    dogs += 5
+if people >= dogs:
+    print "People are greater than or equal to dogs."
+if people <= dogs:
+    print "People are less than or equal to dogs."
+if people == dogs:
+    print "People are dogs."
